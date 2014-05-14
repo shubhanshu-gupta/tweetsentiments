9cb5e38f77b44a978dee8d0a52def73f38b9bc72
Shubhanshu Gupta <shubhanshu.gupta93@gmail.com>
2014-05-14 22:27:10 +0530

    fetch tweets related to the keyword supplied in the search field

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php include "library/twitteroauth.php"; ?>
+<?php
+	$consumer = "NRfvURBas1Rr4pW9FCYXPlMa3";
+	$consumersecret = "YSskWJlgZw1uai9PxpJwJ11rddRIEpOQ8CdbkodkZKXmn820lF";
+	$accesstoken = "69014652-vvtzyNWRorejiSo4Y3X9ezvqk0viXZtqoXDoTvTbv";
+	$accesstokensecret = "VcX1Hzqsjortt90kSkoYnuooXJ3dfZ4bPYUpXhCGspig3";
+
+$twitter = new TwitterOAuth($consumer,$consumersecret,$accesstoken,$accesstokensecret);
+
+$tweets = $twitter->get('https://api.twitter.com/1.1/search/tweets.json?q=php&result_type=mixed&count=10')
+
+?>
+
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<title>Shubhanshu Gupta | Tweet Fetcher</title>
+	</head>
+
+	<body>
+		<form action="" method="post">
+		<label>Search: <input type="text" name="keyword"/></label>
+		</form>
+		<?php 
+		if(isset($_POST['keyword'])){
+			$tweets = $twitter->get('https://api.twitter.com/1.1/search/tweets.json?q='.$_POST['keyword'].'&lang=en&result_type=mixed&count=100');
+			foreach ($tweets as $tweet) {
+							# code...
+							foreach ($tweet as $t) {
+								# code...
+									echo '<img src="'.$t->user->profile_image_url.'" />'.$t->text;
+									echo "<br>";
+							}
+						
+
+						}			
+		}
+		?>
+	</body>
+
+</html>
